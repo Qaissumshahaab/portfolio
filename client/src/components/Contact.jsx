98f0224e3e1bc451c9b01a0a6fd30aaee50d2c6a
Qaissumshahaab <qaissumshahaab@gmail.com>
2026-04-29 23:17:37 +0500

    added backend domain of vercel in frontend

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -87,7 +87,7 @@ const Contact = () => {
     setErrMsg('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch('https://portfoliobackend-two-mocha.vercel.app/api/contact', {
         method:  'POST',
         headers: { 'Content-Type': 'application/json' },
         body:    JSON.stringify(form),
